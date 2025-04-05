0f9d19ff63356ac77f660323e4a56ae63861f2ef
Cadu-L <carlosedulima.mec@hotmail.com>
2025-04-05 13:58:25 -0300

    commit v0.4

--- projeto.py
+++ projeto.py
@@ -1,5 +1,5 @@
-print('Produtos terão 5% de desconto sobre o seu valor.')
+print('Produtos terão 7% de desconto sobre o seu valor.')
 valor = float(input('Digite o valor do produto, R$:'))
-desconto = (5 / 100) * valor
+desconto = (7 / 100) * valor
 total = valor - desconto
 print('O total de desconto sobre o valor de R${:.2f} foi de {:.2f} e seu valor final é de {:.2f}'.format(valor, desconto, total))
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 Teste de funcionamento do GIt. 
 Chave snow.
 Alteração para projeto v0.1
+Ainda não tenho a ideia certo do que vou fazer ainda ou quando vou fazer também. 
