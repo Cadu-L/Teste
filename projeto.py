7b4353755ace5f8fc87b1d4d24e3aba88543cde2
Cadu-L <carlosedulima.mec@hotmail.com>
2025-04-05 08:32:14 -0300

    commit v0.3

--- projeto.py
+++ projeto.py
@@ -1,2 +1,5 @@
-nome = input('Qual é o seu nome?:')
-print('Olá {} seja bem vindo ao projeto python!'.format(nome))
+print('Produtos terão 5% de desconto sobre o seu valor.')
+valor = float(input('Digite o valor do produto, R$:'))
+desconto = (5 / 100) * valor
+total = valor - desconto
+print('O total de desconto sobre o valor de R${:.2f} foi de {:.2f} e seu valor final é de {:.2f}'.format(valor, desconto, total))
